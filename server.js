2ba39c5f3f45714b59644c3fbaabe6e448f20f33
skrzero <Formationafec@outlook.fr>
2025-07-16 15:47:30 +0200

    server

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ app.use(express.json());
 const films = require('./data/films.json');
 const projections = require('./data/projections.json');
 const realisateurs = require('./data/realisateurs.json');
+const { stringify } = require('querystring');
 
 // app.use(express.static("public"));
+
 
 // Route d'accueil
 app.get('/', (req, res) => {
@@ -31,19 +33,37 @@ app.get('/', (req, res) => {
 // GET /films/:id — détail d'un film
 app.get('/films/:id', (req, res) => {
     const filmId = parseInt(req.params.id);
+    if(isNaN(filmId)){
+        res.status(400);
+        res.send('bad request');
+    };
     const film = films.find(f => f.id === filmId);
-    res.json(film)
+    if (!film){
+        res.status(404);
+        res.send('not found');
+    } else {
+        res.json(film);
+    }
+    
 });
 
 //DELETE /film/:id - Supression d'un film
 app.delete('/films/:id', (req, res) => {
-    const filmId = parseInt(req.params.id);  
-    const newListFilms = films.splice(filmId-1, 1);
+    const filmId = parseInt(req.params.id);
+    if(isNaN(filmId)){
+        res.status(400);
+        res.send('bad request');
+    };
+    if (!filmId){
+        res.status(404);
+        res.send('not found');
+    };
+    films.splice(filmId-1, 1);
     
     const dataPath = path.join(__dirname, 'data', 'films.json');
-    fs.writeFileSync(dataPath, JSON.stringify(newListFilms))
+    fs.writeFileSync(dataPath, JSON.stringify(films))
     res.status(204).end();
-})
+});
 
 newFilm = {
     "id": 6,
@@ -58,12 +78,19 @@ newFilm = {
 // POST /film - Ajout d'un film
 app.post('/films', (req, res) => {
     const nouveauFilm = req.body;
+    if(!req.body.titre){
+        res.status(400).send('bad request');
+    };
     
     films.push(nouveauFilm);
     
     const dataPath = path.join(__dirname, 'data', 'films.json');
-    fs.writeFileSync(dataPath, JSON.stringify(films))
-    res.status(201).json({ message: "Film ajouté avec succès", film: nouveauFilm });
+    try{
+        fs.writeFileSync(dataPath, JSON.stringify(films))
+        res.status(201).json({ message: "Film ajouté avec succès", film: nouveauFilm });
+    } catch (error){
+        res.status(500).send('internal error');
+    }
 })
 
 
@@ -74,14 +101,26 @@ const titre =
 // PATCH /films/:id - Modification d'un titre de film 
 app.patch('/films/:id', (req, res) => {
     const filmId = parseInt(req.params.id);
-    const { titre } = req.body;
+    const titre  = req.body.titre;
 
-    const index = filmId-1
-    films[index].titre = titre;
+    const film = films.find(f => f.id === filmId);
+    
+    films.forEach((element,i)=>{
+        if(element.id === filmId){
+            films[i].titre = titre;
 
-    fs.writeFileSync(dataPath, JSON.stringify(films))
-    res.status(200).json({ message: "Film mis à jour avec succès", film: films[index] });
+        }
+    });
+
+    const dataPath = path.join(__dirname,"data/films.json");
+    fs.writeFileSync(dataPath, JSON.stringify(films));
+    res.status(200).json({ message: "Film mis à jour avec succès", film: film });
 });
+
+// app.use((err, req, res, next) => {
+//   console.error("Erreur attrapée :", err.stack);
+//   res.status(500).json({ message: "Erreur interne du serveur" });
+// });
 
 
 const PORT = 3000;
